388ee827c690a90705b44f05b8f54cb7bf54b8cd
Matúš Čongrády <matus.congrady@cellense.com>
2019-04-12 10:56:21 +0200

    remove annoying console.logs

--- engine.js
+++ engine.js
@@ -1,14 +1,13 @@
-var argv = require('yargs').argv;
-var Bluebird = require('bluebird');
-var path = require('path');
+var argv = require("yargs").argv;
+var Bluebird = require("bluebird");
+var path = require("path");
 
 // since old versions of node can't do Object.assign :(
-var mergeObjects = require('./utils/mergeObjects');
+var mergeObjects = require("./utils/mergeObjects");
 
 var requiringModule = module.parent.parent;
 
 module.exports = function(inputOptions) {
-
   var defaultOptions = {
     printOutputFrame: true
   };
@@ -17,25 +16,19 @@ module.exports = function(inputOptions) {
 
   function printOutput(returnValue) {
     Bluebird.resolve(returnValue)
-    .then(function (output) {
-      if (options.printOutputFrame) {
-        console.log('--------make-runnable-output--------');
-      }
-      console.log(output);
-      if (options.printOutputFrame) {
-        console.log('------------------------------------');
-      }
-    }).catch(printError);
+      .catch(function() {
+        return null;
+      });
   }
 
   function printError(error) {
-      if (options.printOutputFrame) {
-        console.log('--------make-runnable-error--------');
-      }
-      console.log(error);
-      if (options.printOutputFrame) {
-        console.log('------------------------------------');
-      }
+    if (options.printOutputFrame) {
+      console.log("--------make-runnable-error--------");
+    }
+    console.log(error);
+    if (options.printOutputFrame) {
+      console.log("------------------------------------");
+    }
   }
 
   // if the requiring file is being run directly from the command line
@@ -43,12 +36,14 @@ module.exports = function(inputOptions) {
     var targetPropertyFound = false;
 
     function runFuncWithArgs(func, unnamedArgs) {
-      var namedArgs = Object.assign({},argv);
+      var namedArgs = Object.assign({}, argv);
       delete namedArgs._;
       delete namedArgs.$0;
 
       if (Object.keys(namedArgs).length > 0 && unnamedArgs.length > 0) {
-        console.error('You cannot specify both named and unnamed arguments to the function');
+        console.error(
+          "You cannot specify both named and unnamed arguments to the function"
+        );
         process.exit(1);
       } else if (Object.keys(namedArgs).length > 0) {
         //we have named arguments. Let's pass these as an object to the function
@@ -75,9 +70,14 @@ module.exports = function(inputOptions) {
 
         // if the target is a function, we need to run it with any provided args
         if (requiringModule.exports[targetProperty] instanceof Function) {
-
-          runFuncWithArgs(requiringModule.exports[targetProperty].bind(requiringModule.exports), argv._.slice(1));
-        } else {// if the target isn't a function, we simply print it
+          runFuncWithArgs(
+            requiringModule.exports[targetProperty].bind(
+              requiringModule.exports
+            ),
+            argv._.slice(1)
+          );
+        } else {
+          // if the target isn't a function, we simply print it
           printOutput(requiringModule.exports[targetProperty]);
         }
       }
@@ -87,36 +87,49 @@ module.exports = function(inputOptions) {
     if (!targetPropertyFound) {
       var validProperties = Object.keys(requiringModule.exports);
       if (validProperties.length === 0) {
-        printError("The module you're trying to make runnable doesn't export anything.");
-      } else { // let's give an example of how this should be used
+        printError(
+          "The module you're trying to make runnable doesn't export anything."
+        );
+      } else {
+        // let's give an example of how this should be used
         var validFunctionProperties = validProperties.filter(function(prop) {
-          return requiringModule.exports[prop] instanceof Function
+          return requiringModule.exports[prop] instanceof Function;
         });
 
-
         // ideally the examples should use a function so it makes sense
-        var egProperty = validFunctionProperties.length > 0 ? validFunctionProperties[0] : validProperties[0];
+        var egProperty =
+          validFunctionProperties.length > 0
+            ? validFunctionProperties[0]
+            : validProperties[0];
 
         // the example changes based on whether the property name contains a space
-        var egPropertyHasSpace = egProperty.indexOf(' ') > 0; //if example property
-        var egPropertyAsArg = egPropertyHasSpace ? '"' + egProperty + '"' : egProperty;
+        var egPropertyHasSpace = egProperty.indexOf(" ") > 0; //if example property
+        var egPropertyAsArg = egPropertyHasSpace
+          ? '"' + egProperty + '"'
+          : egProperty;
 
-        var example = 'node ' + argv.$0 + ' ' + egPropertyAsArg + ' xyz';
-        example += ' → ';
-        example += 'module.exports' + (egPropertyHasSpace ? '[' + egPropertyAsArg + ']': '.' + egProperty) + '("xyz")'
+        var example = "node " + argv.$0 + " " + egPropertyAsArg + " xyz";
+        example += " → ";
+        example +=
+          "module.exports" +
+          (egPropertyHasSpace
+            ? "[" + egPropertyAsArg + "]"
+            : "." + egProperty) +
+          '("xyz")';
 
-        var errorString = 'One of the following must be specified as an argument, to run/print the corresponding function/property:';
-        errorString += '\n\n' + Object.keys(requiringModule.exports);
-        errorString += '\n\nExample:';
-        errorString += '\n\t' + example;
+        var errorString =
+          "One of the following must be specified as an argument, to run/print the corresponding function/property:";
+        errorString += "\n\n" + Object.keys(requiringModule.exports);
+        errorString += "\n\nExample:";
+        errorString += "\n\t" + example;
 
         printError(errorString);
       }
     }
   } else {
     // needed with nested make-runnables
-    delete require.cache[path.join(__dirname, 'engine.js')]
-    delete require.cache[path.join(__dirname, 'index.js')]
-    delete require.cache[path.join(__dirname, 'custom.js')]
+    delete require.cache[path.join(__dirname, "engine.js")];
+    delete require.cache[path.join(__dirname, "index.js")];
+    delete require.cache[path.join(__dirname, "custom.js")];
   }
-}
+};
